5dd3cf5853bc4593387c14e60cba6b07a6223461
ZWX <allenwangyu0311@gmail.com>
2013-09-04 13:28:56 +0800

    增加了工厂模式以及安全构造器

---
+++ factory_pattern.js
@@ -0,0 +1,14 @@
+function createPerson(name, age, job){
+	var o = new Object();
+	o.name = name;
+	o.age = age;
+	o.job = job;
+	o.sayName = function(){
+		alert(this.name);
+	};
+	return o; 
+}
+
+var person = createPerson('allen', 12, 'student');
+
+
